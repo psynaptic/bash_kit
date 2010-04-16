84a8b6f0042a33257b294a2042432b0621363cc0
Balazs Dianiska <balazs@longlake.co.uk>
2010-04-16 16:08:46 +0100

    Adding a prepare script, helping to prepare drupal installations.

---
+++ bin/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Prepare a drupal installation by setting permissions, copying files.
+# Assume we are in the drupal root.
+# TODO verify we are in the drupal root, use path argument if not.
+
+CORE=${1:-6}
+
+cp sites/default/default.settings.php sites/default/settings.php
+chmod 666 sites/default/settings.php
+mkdir sites/default/files
+chmod 777 sites/default/files
+
+if [ $CORE == 7 ]
+then
+  mkdir -p sites/default/private/{files,temp}
+  chmod 777 sites/default/private/files
+fi

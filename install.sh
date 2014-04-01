60ba76a672b7243473bdcabcb1e4d022a00f40a7
Richard Burford <rich@freestylesystems.co.uk>
2014-04-01 18:01:12 +0100

    Reworked installer to use HOME env var in the profile.

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 set -e
 
-export BASH_KIT_DIR=$HOME/.kit
+export BASH_KIT_DIR=".kit"
+export BASH_KIT_INSTALL="$HOME/$BASH_KIT_DIR"
 
-if [ -d $BASH_KIT_DIR ]; then
-  echo "\033[0;31mBash Kit is already installed. Please remove $BASH_KIT_DIR if you want to re-install.\033[0m"
+if [ -d "$BASH_KIT_INSTALL" ]; then
+  echo "\033[0;31mBash Kit is already installed. Please remove $BASH_KIT_INSTALL if you want to re-install.\033[0m"
   exit 1
 fi
 
@@ -11,33 +12,32 @@ echo "\033[0;33mInstalling Bash Kit...\033[0m"
 hash git &>/dev/null || {
   echo "\033[0;31mgit not installed\033[0m"
 }
-git clone https://github.com/psynaptic/bash_kit.git $BASH_KIT_DIR
+git clone https://github.com/psynaptic/bash_kit.git "$BASH_KIT_INSTALL"
 
 # Calculate the bash profile file location.
 if [ -f "$HOME/.profile" ]; then
-  PROFILE="$HOME/.profile"
+  PROFILE=".profile"
 elif [ -f "$HOME/.bash_profile" ]; then
-  PROFILE="$HOME/.bash_profile"
+  PROFILE=".bash_profile"
 elif [ -f "$HOME/.bashrc" ]; then
-  PROFILE="$HOME/.bashrc"
+  PROFILE=".bashrc"
 fi
 
-cat << EOF >> $PROFILE
+cat << EOF >> "$HOME/$PROFILE"
 
-export BASH_PROFILE=$PROFILE
-export BASH_KIT_DIR=$BASH_KIT_DIR
+export BASH_PROFILE="\$HOME/$PROFILE"
+export BASH_KIT_DIR="\$HOME/$BASH_KIT_DIR"
 
-if [ -d \$BASH_KIT_DIR ]; then
+if [ -d "\$BASH_KIT_DIR" ]; then
   if [ "\$BASH" -o "\$ZSH" ]; then
-    if [ -f $BASH_KIT_DIR/profile ]; then
-      . $BASH_KIT_DIR/profile
+    if [ -f "\$HOME/$BASH_KIT_DIR/profile" ]; then
+      . "\$HOME/$BASH_KIT_DIR/profile"
     fi
   fi
 fi
 
 EOF
 
-echo "\033[0;33mSourcing $PROFILE\033[0m"
-. $PROFILE
+echo "\033[0;35mTo complete the installation run the following command:\nsource $HOME/$PROFILE\033[0m"
 
 echo "\033[0;32mDone\033[0m"
